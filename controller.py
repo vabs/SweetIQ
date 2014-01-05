a6caa7399acfa2c0bd2eddaaed0b63fa9a69bf2d
Syed <syed1.mushtaq@gmail.com>
2014-01-05 18:22:33 -0500

    adding find

--- controller.py
+++ controller.py
@@ -1,15 +1,24 @@
 import os
-from flask import Flask,render_template
+from flask import Flask,render_template,session
 import random
+import uuid
+import json
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-
     return render_template('home.html')
 
+
+
+@app.route('/find', methods = ['GET', 'POST'])
+def find_location():
+
+    print request.form
+    
+    session['id'] = uuid.uuid4().hex
 
 
 
