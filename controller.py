30f77a130ff8b486f6a17d614ac6f1f4f7397b20
Syed <syed1.mushtaq@gmail.com>
2014-01-05 21:17:45 -0500

    adding stuff

--- controller.py
+++ controller.py
@@ -84,8 +84,7 @@ def get_data(token_id):
     listings = Listing.query.filter(Listing.location_id==token_id).all()
 
     return render_template('listings.html', all_listings={ 'name': location_name, 
-                    'listings':listings 
-            }})
+                    'listings':listings })
 
 
 
